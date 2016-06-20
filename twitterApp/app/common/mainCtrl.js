fd1691d7d8455ef4650318bc0ee5341649aebd15
kiran paturi <kpaturi@smarttuition.com>
2016-06-20 16:06:18 +0530

    added code

--- twitterApp/app/common/mainCtrl.js
+++ twitterApp/app/common/mainCtrl.js
@@ -6,14 +6,30 @@ angular.module('twitterApp')
 
 
         $scope.loadTemplate = function (type) {
-            if (type == 'login') {
-                $scope.templateUrl = loginUrl;
-            } else if (type == 'register') {
-                $scope.templateUrl = registerUrl;
-            } else if (type == 'tweets') {
-                $scope.templateUrl = tweetUrl;
-            }
+            /* if (type == 'login') {
+                 $scope.templateUrl = loginUrl;
+             } else if (type == 'register') {
+                 $scope.templateUrl = registerUrl;
+             } else if (type == 'tweets') {
+                 $scope.templateUrl = tweetUrl;
+             }*/
+            $scope.templateUrl = type.template;
         };
         $scope.headerUrl = "app/common/navbar.tpl.html";
+
+        $scope.tabs = [
+            {
+                name: "Login",
+                template: 'app/login/login.tpl.html'
+                },
+            {
+                name: "Register",
+                template: 'app/register/register.tpl.html'
+            },
+            {
+                name: "Tweets",
+                template: 'app/tweets/tweet.tpl.html'
+            },
+                      ]
 
     });
